c686e568c5dda3bad1d56148589ea1b09709189f
Irfan Badzlin <irfan.badrul11@s.unikl.edu.my>
2022-07-13 21:29:24 +0800

    FIx

--- coolscan.py
+++ coolscan.py
@@ -14,7 +14,7 @@ from platform import system
 def sigint_handler(signum, frame):
     os.system("clear")
     print ("CTRL+C detected!")
-    print(" \033[1;91m@Good bye\033[1;m")
+    print(" \033[1;91m@Good bye !\033[1;m")
     sys.exit() 
  
 signal.signal(signal.SIGINT, sigint_handler)
@@ -354,7 +354,7 @@ def baslangic():
             baslangic()
 
     if selection =="0":
-        print(" \033[1;91mGoodbye\033[1;m")
+        print(" \033[1;91mGoodbye !\033[1;m")
         sys.exit()
 
     else:
